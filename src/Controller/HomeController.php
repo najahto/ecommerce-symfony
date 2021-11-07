864975b6e8d0561b1bc6579d44a97e353008932a
NAJAH Taha Omar <tahaomarnajah@gmail.com>
2021-11-07 06:35:24 +0100

    feat: display products on frontend

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,23 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     /**
+     * @var ProductRepository
+     */
+    private $repository;
+
+    public function __construct(ProductRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    /**
      * @Route("/", name="home")
      */
     public function index(): Response
     {
-        return $this->render('home/index.html.twig');
+        $products = $this->repository->findLatest();
+        return $this->render('home/index.html.twig', [
+            'latestProducts' => $products
+        ]);
     }
 }
